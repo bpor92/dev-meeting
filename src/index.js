40b3575d4f0bafe443e500ec2486c75ba506aab3
Bartek <bartoszporebski1992@gmail.com>
2018-12-09 09:23:28 +0100

    fix css import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import {render} from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 
-import './css/app.css'
 import * as serviceWorker from './serviceWorker';
 
 render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
